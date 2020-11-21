948564b964d20c941e7de8945067c65fe8fd6a05
Gianni Carlini <gianni.carlini@sansano.usm.cl>
2020-11-21 01:41:51 -0300

    agregado un par de cosas grpc

---
+++ dataNode1.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"io"
+	"log"
+	"net"
+
+	"google.golang.org/grpc"
+
+	pb "github.com/GianniCarlini/Lab-2-SD/proto"
+)
+const (
+	port = ":50051"
+	port2 = ":50052"
+)
+
+type server struct {
+}
+
+func (s *server) EnviarLibro(stream pb.Packet_EnviarLibroServer) error {
+	log.Println("Started stream")
+	for {
+		in, err := stream.Recv()
+		log.Println("Received value")
+		if err == io.EOF {
+			return nil
+		}
+		if err != nil {
+			return err
+		}
+		log.Println("Got "+in.Id)
+	}
+}
+
+func main() {
+	lis, err := net.Listen("tcp", port)
+
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+
+	s := grpc.NewServer()
+
+	pb.RegisterPacketServer(s, &server{})
+
+	if err := s.Serve(lis); err != nil {
+		log.Fatalf("failed to serve: %v", err)
+	}
+}
